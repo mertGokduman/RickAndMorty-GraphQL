52b13b653ea396ef279b510b758b308365d6ee1f
Mert Gökduman <mert.gokduman@external.yapikredi.com.tr>
2022-06-26 18:43:17 +0300

    Initial Commit

---
+++ RickAndMorty-GraphQL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty-GraphQL
+//
+//  Created by Mert Gökduman on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
